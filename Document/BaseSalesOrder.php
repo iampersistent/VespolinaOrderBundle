44455ec73502361313c485667d45e4980b0cf93b
Inspiran <daniel@inspiran.be>
2011-11-27 22:42:06 +0100

    Sales order id now uses doctrine auto increment for incrementing ids

--- Document/BaseSalesOrder.php
+++ Document/BaseSalesOrder.php
@@ -14,4 +14,15 @@ use Vespolina\OrderBundle\Model\SalesOrder as AbstractSalesOrder;
 abstract class BaseSalesOrder extends AbstractSalesOrder
 {
     protected $id;
+
+    public function getId()
+    {
+
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 }
